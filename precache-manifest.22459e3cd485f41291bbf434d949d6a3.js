86a69bd17a7eade9ea83964a5afb4dc7e94aaa90
H4kan <szymon.sieradzki184@wp.pl>
2019-04-02 20:01:11 +0200

    Updates

---
+++ precache-manifest.22459e3cd485f41291bbf434d949d6a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b974a03c4539c47a8c6715b45c6c72a",
+    "url": "/mypage/static/media/projectEnigma.1b974a03.png"
+  },
+  {
+    "revision": "9d14e5a7a3af0606417a35b4b68efea6",
+    "url": "/mypage/static/media/profile.9d14e5a7.jpg"
+  },
+  {
+    "revision": "53bd1d142d66b8ee5efed5f4981670e6",
+    "url": "/mypage/static/media/image1.53bd1d14.jpg"
+  },
+  {
+    "revision": "1d0eb0fb2dd27e69d724",
+    "url": "/mypage/static/js/runtime~main.4c2b4ee8.js"
+  },
+  {
+    "revision": "6b3617787bd9e687f298",
+    "url": "/mypage/static/js/main.42f792ac.chunk.js"
+  },
+  {
+    "revision": "83352b64c5a61b73d972",
+    "url": "/mypage/static/js/2.42667289.chunk.js"
+  },
+  {
+    "revision": "6b3617787bd9e687f298",
+    "url": "/mypage/static/css/main.734e22ac.chunk.css"
+  },
+  {
+    "revision": "e489cdd58031e7ef5cb48b69006f7390",
+    "url": "/mypage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f950aa36aee5b8e983885e1777dadc9.js"
+  "/mypage/precache-manifest.22459e3cd485f41291bbf434d949d6a3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mypage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
